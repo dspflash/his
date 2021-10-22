7c6efa09934920c98c5faff5c210cdea552b6d50
dspflash <sardine@3ucs.com>
2021-10-22 16:58:42 +0800

    initial

---
+++ install/reset_trans.sql
@@ -0,0 +1,12 @@
+use his;
+TRUNCATE TABLE bottle_info;
+TRUNCATE TABLE bottle_loc_cabinet;
+TRUNCATE TABLE bottle_log;
+TRUNCATE TABLE bottle_transaction;
+TRUNCATE TABLE instock_log;
+TRUNCATE TABLE medi_trans;
+TRUNCATE TABLE medicine_loc_cabinet;
+TRUNCATE TABLE medication;
+TRUNCATE TABLE medication_detail;
+TRUNCATE TABLE medicineequipment_base;
+TRUNCATE TABLE menu;
