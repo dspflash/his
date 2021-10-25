3e305f9c8f0d622de51abf3f38e92c2f45e9e4af
dspflash <sardine@3ucs.com>
2021-10-25 15:39:55 +0800

    adjust

--- dev/dump_his.bat
+++
@@ -1,4 +0,0 @@
-echo on
-mysqldump -u root -p --databases his >his.sql --triggers --routines
-::mysqldump -u root -p --databases vtigercrm6 >vtigerpostgresql.sql --compatible=postgresql --default-character-set=utf8 --triggers --routines
-pause
